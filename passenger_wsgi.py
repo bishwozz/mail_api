de992706cb4d2ea7bf3e5b09d1fe829ba42413f9
bishwozz <bishwo123sapkota@gmail.com>
2025-02-18 14:03:41 +0545

    link file created

---
+++ passenger_wsgi.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Add the directory where 'main.py' is located to sys.path
+sys.path.insert(0, '/home/codezfvv/python_pro/mail_api')
+
+# Import the FastAPI app object from main.py
+from main import app as application
