f5f9391dd3815b94f1f03fceab0dc5647e2a87df
bishwozz <bishwo123sapkota@gmail.com>
2025-02-18 14:19:44 +0545

    fixed path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,7 @@
 import sys
 import os
 
-# Add the directory where 'main.py' is located to sys.path
-sys.path.insert(0, '/home/codezfvv/python_pro/mail_api')
+# Set the path to your application
+sys.path.insert(0, '/home/codezfvv/mail_api')
 
-# Import the FastAPI app object from main.py
-from main import app as application
+from main import app as application  # Assuming 'app' is the FastAPI instance in main.py
